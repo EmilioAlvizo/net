07ab6a38983dbbf27aa3303189a9c61a758418eb
Emilio Alvizo Velazquez <emilio_alvizo@yahoo.com.mx>
2026-03-19 10:09:31 -0600

    ejercicios

---
+++ WebApplication1/ejercicio.cs
@@ -0,0 +1,9 @@
+// ejercicio1.cs
+public class ejercicio1
+{
+    public static void Correr()
+    {
+        Console.WriteLine("Hola desde mis ejercicios!");
+        // todo tu código de prueba aquí
+    }
+}
