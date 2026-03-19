ff133bff6bffa7a65fa50425effffc7e287c2a07
Emilio Alvizo Velazquez <emilio_alvizo@yahoo.com.mx>
2026-03-19 11:55:46 -0600

    uso de librerias

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -50,10 +50,14 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 });
 
+ejercicio1.Correr();
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+
 
--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d00f835994a5bfe71585cf25dc9c636ad587e7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07ab6a38983dbbf27aa3303189a9c61a758418eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
