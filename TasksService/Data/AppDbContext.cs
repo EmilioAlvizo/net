793a5939b98065ed74153eb6c2ac386f01f72367
Emilio Alvizo Velazquez <emilio_alvizo@yahoo.com.mx>
2026-03-20 14:56:28 -0600

    esto es para traer la tabla Clientes

--- TasksService/Data/AppDbContext.cs
+++ TasksService/Data/AppDbContext.cs
@@ -6,7 +6,6 @@ namespace TasksService.Data;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
-    // Esta línea = tabla "Tasks" en la base de datos
-    public DbSet<TaskItem> Tasks { get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
     
 }
