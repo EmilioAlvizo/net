6e1dc1bd8da1fe46cfe41f83f61dacc75ac85ad8
Emilio Alvizo Velazquez <emilio_alvizo@yahoo.com.mx>
2026-03-20 14:38:45 -0600

    .

---
+++ TasksService/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using TasksService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Servicios
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Aquí conectamos EF Core con SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
